2e5b3a022c1f2b19cee4686318742cb895da7e06
Khoa Nguyen <khoanguyen0109.99@gmail.com>
2021-03-12 17:57:17 +0700

    first commit

---
+++ src/utils/theme/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "",
+    },
+    secondary: {
+      main: "",
+    },
+  },
+  typography: {
+    fontFamily: "Raleway, Arial",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
